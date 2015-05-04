b69f491c8c85cb56f7ab4b5851298189725da05b
Michael Hrcek <mhrcek@imsa.edu>
2015-05-04 10:01:47 -0500

    updated genetype

--- CompuSciHW12/src/main/java/geneticsHomework/GeneType.java
+++ CompuSciHW12/src/main/java/geneticsHomework/GeneType.java
@@ -1,5 +1,48 @@
 package geneticsHomework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum GeneType {
-	MALE, BROWN_COAT;
+
+    MALE(true),
+    BROWN_COAT(false);
+
+    private boolean isDominant;
+    private GeneType[] similarGenes;
+
+    private GeneType(boolean isDominant, GeneType... similarGenes) {
+        this.isDominant = isDominant;
+        this.similarGenes = similarGenes;
+    }
+
+    public boolean isDominant() {
+        return isDominant;
+    }
+
+    public static boolean isDominant(GeneType gt) {
+        return gt.isDominant();
+    }
+
+    public GeneType[] getSimilarGenesArray() {
+        return similarGenes;
+    }
+
+    public List<GeneType> getSimilarGenesList() {
+        List l = new ArrayList();
+
+        for (GeneType gt : similarGenes) {
+            l.add(gt);
+        }
+
+        return l;
+    }
+
+    public static GeneType[] getSimilarGenesArray(GeneType gt) {
+        return gt.getSimilarGenesArray();
+    }
+
+    public static List<GeneType> getSimilarGenesList(GeneType gt) {
+        return gt.getSimilarGenesList();
+    }
 }
